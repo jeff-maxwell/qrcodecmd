820b6708412a3fd5d0405dc68f05ba96be8b57eb
jeff <jeff@tcnsolutions.com>
2023-12-29 10:24:05 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using QRCoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.okcu.edu", QRCodeGenerator.ECCLevel.Q);
+        QRCode qrCode = new QRCode(qrCodeData);
+        Bitmap qrCodeImage = qrCode.GetGraphic(20);
+        qrCodeImage.Save("test.png");
+    }
+}
